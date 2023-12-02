d97b3cc1c575f95e9b223b112e30df32314475c4
Amin <bihamtaamin81@gmail.com>
2023-12-02 21:09:25 +0800

    First working version

--- Script.js
+++ Script.js
@@ -1,7 +1,45 @@
+globalNum = 0.0;
+globalAction = "none";
+
 function inputNumber(num) {
-    document.getElementById('screenNum').innerHTML += num;
+  element = document.getElementById("screenNum");
+  if (globalAction == "=") {
+    element.innerHTML = "";
+  }
+  element.innerHTML += num;
 }
 
 function backspace() {
-    document.getElementById('screenNum').innerHTML = document.getElementById('screenNum').innerHTML.slice(0, -1);
+  document.getElementById("screenNum").innerHTML = document
+    .getElementById("screenNum")
+    .innerHTML.slice(0, -1);
+}
+
+function addNumber(action) {
+  element = document.getElementById("screenNum");
+  if (globalNum == 0.0) {
+    globalNum = parseFloat(element.innerHTML);
+    element.innerHTML = "";
+    globalAction = action;
+  }
+}
+
+function equal() {
+  element = document.getElementById("screenNum");
+  switch (globalAction) {
+    case "+":
+      element.innerHTML = globalNum + parseFloat(element.innerHTML);
+      break;
+    case "-":
+      element.innerHTML = globalNum - parseFloat(element.innerHTML);
+      break;
+    case "*":
+      element.innerHTML = globalNum * parseFloat(element.innerHTML);
+      break;
+    case "/":
+      element.innerHTML = globalNum / parseFloat(element.innerHTML);
+      break;
+  }
+  globalNum = 0.0;
+  globalAction = "=";
 }
